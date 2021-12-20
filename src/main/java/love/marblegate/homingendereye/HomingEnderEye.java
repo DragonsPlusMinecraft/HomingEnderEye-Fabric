c86219b1af2419db18bb1ab7d9374520a1a1233f
白门守望者 <marblegatekeeper@outlook.com>
2021-12-20 09:47:33 +0800

    Update to 1.0.1
    [update] Add Mod Menu integration.
    [fix] Mod name is now correct.

--- src/main/java/love/marblegate/homingendereye/HomingEnderEye.java
+++ src/main/java/love/marblegate/homingendereye/HomingEnderEye.java
@@ -7,8 +7,6 @@ import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
-
-
 public class HomingEnderEye implements ModInitializer {
 
     public static EyeThrowCache EYE_THROW_CACHE;
